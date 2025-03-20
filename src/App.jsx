df87537b173995bf99b7b72903a148d8b2c70c0a
GiuseppeColucci95 <colucci539@gmail.com>
2025-03-20 16:04:12 +0100

    Update index.html and add Header component with styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 
+import Header from './components/Header';
+
 function App() {
 
   return (
     <>
-      prova
+      <Header></Header>
     </>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,26 @@
+export default function Header() {
+
+  return (
+    <header>
+      <div className="container">
+
+        <div className="row">
+          <img src="/dc-logo.png" alt="dc logo" />
+          <ul>
+            <li>CHARACTERS</li>
+            <li>COMICS</li>
+            <li>MOVIES</li>
+            <li>TV</li>
+            <li>GAMES</li>
+            <li>COLLECTIBLES</li>
+            <li>VIDEOS</li>
+            <li>FANS</li>
+            <li>NEWS</li>
+            <li>SHOP</li>
+          </ul>
+        </div>
+
+      </div>
+    </header>
+  );
+}
