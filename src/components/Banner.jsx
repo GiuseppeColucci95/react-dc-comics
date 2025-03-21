38a96008ad1feae0c79461653af8a3896a951a61
GiuseppeColucci95 <colucci539@gmail.com>
2025-03-21 16:19:46 +0100

    Refactor Banner component layout; add Card component for series display; enhance styles in index.css

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -5,23 +5,23 @@ export default function Banner() {
     <section id="banner">
       <div className="container">
         <div className="row">
-          <div className="col">
+          <div className="col-5">
             <img src="/buy-comics-digital-comics.png" alt="digital image" />
             <span>DIGITAL COMICS</span>
           </div>
-          <div className="col">
+          <div className="col-5">
             <img src="/buy-comics-merchandise.png" alt="merchandise image" />
             <span>DC MERCHANDISE</span>
           </div>
-          <div className="col">
+          <div className="col-5">
             <img src="/buy-comics-subscriptions.png" alt="subscription image" />
             <span>SUBSCRIPTION</span>
           </div>
-          <div className="col">
+          <div className="col-5">
             <img src="/buy-comics-shop-locator.png" alt="shop locator image" />
             <span>COMIC SHOP LOCATOR</span>
           </div>
-          <div className="col">
+          <div className="col-5">
             <img src="/buy-dc-power-visa.svg" alt="power image" />
             <span>DC POWER VISA</span>
           </div>
---
+++ src/components/Card.jsx
@@ -0,0 +1,13 @@
+export default function Card({ image, title }) {
+
+  return (
+    <div className="col-2">
+      <div className="card">
+        <div className="img-container">
+          <img src={image} alt={`${title} image`} />
+        </div>
+        <div className="title">{title}</div>
+      </div>
+    </div>
+  );
+}
