3f2f4be04fc4abad4bd34c2904b2e6d00f66f8b2
GiuseppeColucci95 <colucci539@gmail.com>
2025-03-24 12:57:20 +0100

    Add links data and pass to Header component

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,14 @@ import Main from './components/Main';
 import Banner from './components/Banner';
 import Footer from './components/Footer';
 
+import comics from './data/comics';
+import links from './data/links';
+
 function App() {
 
   return (
     <>
-      <Header></Header>
+      <Header content={links}></Header>
       <Main></Main>
       <Banner></Banner>
       <Footer></Footer>
---
+++ src/data/links.js
@@ -0,0 +1,52 @@
+const links = [
+  {
+    id: 1,
+    href: '#',
+    title: 'CHARACTERS',
+  },
+  {
+    id: 2,
+    href: '#',
+    title: 'COMICS',
+  },
+  {
+    id: 3,
+    href: '#',
+    title: 'MOVIES',
+  },
+  {
+    id: 4,
+    href: '#',
+    title: 'TV',
+  },
+  {
+    id: 5,
+    href: '#',
+    title: 'GAMES',
+  },
+  {
+    id: 6,
+    href: '#',
+    title: 'COLLECTIBLES',
+  },
+  {
+    id: 7,
+    href: '#',
+    title: 'VIDEOS',
+  },
+  {
+    id: 8,
+    href: '#',
+    title: 'FANS',
+  },
+  {
+    id: 9,
+    href: '#',
+    title: 'NEWS',
+  },
+  {
+    id: 10,
+    href: '#',
+    title: 'SHOP',
+  }
+];
